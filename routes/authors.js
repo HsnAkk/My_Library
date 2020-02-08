c1d9c8275ad285e39f558f5e5fe4ce545095c7f8
HsnAkk <hasan.akkas@web.de>
2020-02-08 19:18:18 +0100

    set up author router

---
+++ routes/authors.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+
+const Author = require('../models/author');
+
+
+// All authors route
+// authors/
+router.route('/')
+    .get(async (req, res) => { 
+
+        let searchOptions = {}
+        if (req.query.name != null && req.query.name != '') {
+            searchOptions.name = new RegExp(req.query.name, 'i')
+        }
+
+        try {
+            const authors = await Author.find(searchOptions)
+            res.render('authors/index', { 
+                authors : authors,
+                searchOptions : req.query
+            })
+
+        } catch (error) {
+            res.redirect('/')
+        }
+       
+});
+
+
+// new author route
+// authors/new
+router.route('/new')
+    .get((req, res) => { 
+        res.render('authors/new', { author : new Author})
+});
+
+// create author route
+// authors/
+router.route('/')
+    .post( async (req, res) => { 
+        const author = new Author({
+            name : req.body.name
+        })
+
+        try {
+            const newAuthor = await author.save()
+
+            // res.redirect(`authors/${newAuthor.id}`)
+            res.redirect('authors')
+
+        } catch (error) {
+            res.render('authors/new', { author: author, errorMessage: 'Error creating Author'}) 
+        }
+
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const bodyParser = require('body-parser');
 
+// routes links
 const indexRouter = require('./routes/index');
+const authorsRouter = require('./routes/authors');
+
 
 // view engine settings
 app.set('view engine', 'ejs');
@@ -12,6 +16,9 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+
+// bodyParser settings
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}));
 
 // mongoDB settings
 mongoose.connect(process.env.MONGO_URI, {
@@ -23,8 +30,13 @@ const db = mongoose.connection;
 db.on('error', error => console.log(error));
 db.once('open', () => console.log('Connected to MongoDB...'))
 
+
 // routes
 app.use('/', indexRouter);
+app.use('/authors', authorsRouter);
+
+
+
 
 const PORT = process.env.PORT || 4000;
 
